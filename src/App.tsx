fc30f8e3d75185c4af65fc95b86f03dc87accafa
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2025-01-28 16:32:25 +0100

    orga + arborescence

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      <div class='navbar'>
+        <h1>Logo</h1>
+        <ul class='navbarUL'>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+        </ul>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default App;
